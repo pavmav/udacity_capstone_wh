b20ea1c9ba85092d00765167b0c935dba14a991f
Pavel <pavel.mavrichev@gmail.com>
2021-09-24 19:39:34 +0700

    added post items and get items and warehouses

--- app.py
+++ app.py
@@ -1,11 +1,80 @@
 # gunicorn -w 4 app:app --access-logfile -
 
-from flask import Flask
+from flask import Flask, request, abort, jsonify
+from models import db, Warehouse, Item
+import sys
+
 app = Flask(__name__)
 
+app.config.from_object('config')
+db.init_app(app)
+app.app_context().push()
+
+# db.create_all()
+
+# HEALTH CHECK
 @app.route("/")
 def hello():
-    return "<h1 style='color:blue'>Hello There!</h1>"
+    return "Healthy"
+
+# CREATE ENTITIES
+@app.route("/warehouses", methods=['POST'])
+def add_warehouse():
+    
+    # get posted json object
+    wh_data = request.get_json()
+
+    try:
+        new_wh = Warehouse()
+        new_wh.name = wh_data['name']
+        if 'overdraft_control' in wh_data:
+            new_wh.overdraft_control = wh_data['overdraft_control']
+        new_wh.insert()
+
+        return jsonify({
+            'success': True,
+            'new_wh': new_wh.format()
+        })
+
+    except:
+        print(sys.exc_info())
+        abort(400)
+
+@app.route("/items", methods=['POST'])
+def add_item():
+    
+    # get posted json object
+    item_data = request.get_json()
+
+    try:
+        new_item = Item()
+        new_item.name = item_data['name']
+        new_item.insert()
+
+        return jsonify({
+            'success': True,
+            'new_item': new_item.format()
+        })
+
+    except:
+        print(sys.exc_info())
+        abort(400)
+
+@app.route("/warehouses", methods=['GET'])
+def get_warehouses():
+    wh_list = [wh.format() for wh in Warehouse.query.all()]
+
+    return jsonify(wh_list)
+
+@app.route("/items", methods=['GET'])
+def get_items():
+    items_list = [item.format() for item in Item.query.all()]
+
+    return jsonify(items_list)
+
+
+
+
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0',debug=True)
--- models.py
+++ models.py
@@ -7,9 +7,28 @@ class Warehouse(db.Model):
 
     id = db.Column(db.Integer, primary_key=True, nullable=False)
     name = db.Column(db.String(120), nullable=False, unique=True)
+    overdraft_control = db.Column(db.Boolean, default=False, nullable=False)
 
     def __repr__(self) -> str:
         return f'<{self.name} (id:{self.id})>'
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+    
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def format(self):
+        return {
+        'id': self.id,
+        'name': self.name,
+        'overdraft_control': self.overdraft_control
+    }
 
 class Item(db.Model):
     __tablename__ = 'items'
@@ -19,4 +38,21 @@ class Item(db.Model):
 
     def __repr__(self) -> str:
         return f'<{self.name} (id:{self.id})>'
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+    
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def format(self):
+        return {
+        'id': self.id,
+        'name': self.name
+    }
 

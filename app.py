6e1256af228c39ac9175a2114a704b726d887a39
Pavel <pavel.mavrichev@gmail.com>
2021-09-27 20:23:41 +0700

    added overdraft control and get balances

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 # gunicorn -w 4 app:app --access-logfile -
 
 from flask import Flask, request, abort, jsonify
-from models import db, Warehouse, Item
+from models import db, Warehouse, Item, BalanceJournal
 import sys
 
 app = Flask(__name__)
@@ -10,7 +10,7 @@ app.config.from_object('config')
 db.init_app(app)
 app.app_context().push()
 
-# db.create_all()
+db.create_all()
 
 # HEALTH CHECK
 @app.route("/")
@@ -102,6 +102,74 @@ def delete_item(item_id):
         'success': True
     })    
 
+# POST BALANCE OPERATIONS
+@app.route("/balances", methods=['POST'])
+def post_balance_operation():
+    # get posted json object
+    operation_data = request.get_json()
+
+    # should have 3 properties:
+    #   - warehouse_id: int
+    #   - item_id: int
+    #   - quantity: int (positives will be added, negatives will be substracted)
+    if not ('warehouse_id' in operation_data 
+            and 'item_id' in operation_data 
+            and 'quantity' in operation_data):
+        abort(400)
+
+    warehouse_id = operation_data['warehouse_id']
+    item_id = operation_data['item_id']
+    quantity = operation_data['quantity']
+
+    bj_entry = BalanceJournal.query.get((warehouse_id, item_id))
+    new = False
+
+    # if there are no info in the journal, initiate a new entry
+    if bj_entry is None:
+        new = True
+        bj_entry = BalanceJournal()
+        bj_entry.warehouse_id = warehouse_id
+        bj_entry.item_id = item_id
+        bj_entry.quantity = 0
+
+    bj_entry.quantity = bj_entry.quantity + quantity
+
+    # check for overdraft
+    warehouse = Warehouse.query.get(warehouse_id)
+    if bj_entry.quantity < 0 and warehouse.overdraft_control:
+        abort(400)    
+
+    if new:
+        bj_entry.insert()
+    else:
+        bj_entry.update()
+
+    return jsonify({
+        'success': True,
+        'new_balance': bj_entry.quantity
+    })
+
+# GET BALANCES
+@app.route("/balances", methods=['GET'])
+def get_all_balances():
+
+    balances_list_raw = BalanceJournal.query.all()
+
+    balances_list = []
+
+    for entry_raw in balances_list_raw:
+        warehouse_dict = entry_raw.warehouse.format()
+        item_dict = entry_raw.item.format()
+        
+        entry = {
+            'warehouse': warehouse_dict,
+            'item': item_dict,
+            'quantity': entry_raw.quantity
+        }
+
+        balances_list.append(entry)
+
+    return jsonify(balances_list)
 
 
 
--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import backref
 
 db = SQLAlchemy()
 
@@ -8,6 +9,7 @@ class Warehouse(db.Model):
     id = db.Column(db.Integer, primary_key=True, nullable=False)
     name = db.Column(db.String(120), nullable=False, unique=True)
     overdraft_control = db.Column(db.Boolean, default=False, nullable=False)
+    balances = db.relationship('BalanceJournal', backref='warehouse')
 
     def __repr__(self) -> str:
         return f'<{self.name} (id:{self.id})>'
@@ -35,6 +37,7 @@ class Item(db.Model):
 
     id = db.Column(db.Integer, primary_key=True, nullable=False)
     name = db.Column(db.String(120), nullable=False, unique=True)
+    balances = db.relationship('BalanceJournal', backref='item')
 
     def __repr__(self) -> str:
         return f'<{self.name} (id:{self.id})>'
@@ -55,4 +58,33 @@ class Item(db.Model):
         'id': self.id,
         'name': self.name
     }
+
+class BalanceJournal(db.Model):
+    __tablename__ = 'balance_journal'
+
+    warehouse_id = db.Column(db.Integer, db.ForeignKey('warehouses.id'), primary_key=True, nullable=False)
+    item_id = db.Column(db.Integer, db.ForeignKey('items.id'), primary_key=True, nullable=False)
+    quantity = db.Column(db.Integer, nullable=False, default=0)
+
+    def __repr__(self) -> str:
+        return f'<WH:{self.warehouse} I:{self.item} - quantity:{self.quantity})>'
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+    
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def format(self):
+        return {
+        'warehouse': self.warehouse,
+        'item': self.item,
+        'quantity': self.quantity,
+    }
+
 
